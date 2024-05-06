a8717ab3442860841d45323f175a9cfb6b95e5c4
Thisal Lochana <134125776+ThisalLochana@users.noreply.github.com>
2024-05-06 19:09:07 +0530

    Done with the MedicalRecord classes

--- src/main/java/com/mycompany/healthcareapi/netbeans18/dao/MedicalRecordDAO.java
+++ src/main/java/com/mycompany/healthcareapi/netbeans18/dao/MedicalRecordDAO.java
@@ -4,10 +4,50 @@
  */
 package com.mycompany.healthcareapi.netbeans18.dao;
 
+import com.mycompany.healthcareapi.netbeans18.model.MedicalRecord;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author THISAL
  */
 public class MedicalRecordDAO {
-    
+    private static List<MedicalRecord> medicalRecords = new ArrayList<>();
+
+    public List<MedicalRecord> getAllMedicalRecords() {
+        return medicalRecords;
+    }
+
+    public MedicalRecord getMedicalRecord(int id) {
+        for (MedicalRecord medicalRecord : medicalRecords) {
+            if (medicalRecord.getPatient().getId() == id) {
+                return medicalRecord;
+            }
+        }
+        return null;
+    }
+
+    public void updateMedicalRecord(MedicalRecord medicalRecord) {
+        for (MedicalRecord m : medicalRecords) {
+            if (m.getPatient().getId() == medicalRecord.getPatient().getId()) {
+                m.setDiagnosis(medicalRecord.getDiagnosis());
+                m.setTreatment(medicalRecord.getTreatment());
+                break;
+            }
+        }
+    }
+
+    public void deleteMedicalRecord(int id) {
+        MedicalRecord medicalRecordToRemove = null;
+        for(MedicalRecord medicalRecord: medicalRecords){
+            if(medicalRecord.getPatient().getId() == id){
+                medicalRecordToRemove = medicalRecord;
+                break;
+            }
+        }
+        if(medicalRecordToRemove != null){
+            medicalRecords.remove(medicalRecordToRemove);
+        }
+    }
 }
--- src/main/java/com/mycompany/healthcareapi/netbeans18/resources/MedicalRecordResource.java
+++ src/main/java/com/mycompany/healthcareapi/netbeans18/resources/MedicalRecordResource.java
@@ -4,10 +4,61 @@
  */
 package com.mycompany.healthcareapi.netbeans18.resources;
 
+import com.mycompany.healthcareapi.netbeans18.dao.MedicalRecordDAO;
+import com.mycompany.healthcareapi.netbeans18.model.MedicalRecord;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
 /**
  *
  * @author THISAL
  */
+@Path("/medicalRecords")
 public class MedicalRecordResource {
-    
+    private MedicalRecordDAO medicalRecordDAO = new MedicalRecordDAO();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MedicalRecord> getAllMedicalRecords() {
+        return medicalRecordDAO.getAllMedicalRecords();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public MedicalRecord getMedicalRecord(@PathParam("id") int id) {
+        return medicalRecordDAO.getMedicalRecord(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public MedicalRecord createMedicalRecord(MedicalRecord medicalRecord) {
+        medicalRecordDAO.updateMedicalRecord(medicalRecord);
+        return medicalRecord;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public MedicalRecord updateMedicalRecord(@PathParam("id") int id, MedicalRecord medicalRecord) {
+        medicalRecordDAO.updateMedicalRecord(medicalRecord);
+        return medicalRecord;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteMedicalRecord(@PathParam("id") int id) {
+        medicalRecordDAO.deleteMedicalRecord(id);
+    }
 }
