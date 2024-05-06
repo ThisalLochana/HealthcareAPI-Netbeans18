ca6b8dbbbf27b0388c7fed6acb4f777f7c4376f3
Thisal Lochana <134125776+ThisalLochana@users.noreply.github.com>
2024-05-06 19:17:04 +0530

    Done with the Prescription classes

--- src/main/java/com/mycompany/healthcareapi/netbeans18/dao/PrescriptionDAO.java
+++ src/main/java/com/mycompany/healthcareapi/netbeans18/dao/PrescriptionDAO.java
@@ -4,10 +4,56 @@
  */
 package com.mycompany.healthcareapi.netbeans18.dao;
 
+import com.mycompany.healthcareapi.netbeans18.model.Prescription;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author THISAL
  */
 public class PrescriptionDAO {
+    private static List<Prescription> prescriptions = new ArrayList<>();
+
+    public void createPrescription(Prescription prescription) {
+        prescriptions.add(prescription);
+    }
     
+    public List<Prescription> getAllPrescriptions() {
+        return prescriptions;
+    }
+
+    public Prescription getPrescription(int id) {
+        for (Prescription prescription : prescriptions) {
+            if (prescription.getId() == id) {
+                return prescription;
+            }
+        }
+        return null;
+    }
+
+    public void updatePrescription(Prescription prescription) {
+        for (Prescription p : prescriptions) {
+            if (p.getId() == prescription.getId()) {
+                p.setPatient(prescription.getPatient());
+                p.setMedication(prescription.getMedication());
+                p.setDosage(prescription.getDosage());
+                p.setInstructions(prescription.getInstructions());
+                break;
+            }
+        }
+    }
+
+    public void deletePrescription(int id){
+        Prescription prescriptionToRemove = null;
+        for(Prescription prescription: prescriptions){
+            if(prescription.getId() == id){
+                prescriptionToRemove = prescription;
+                break;
+            }
+        }
+        if(prescriptionToRemove != null){
+            prescriptions.remove(prescriptionToRemove);
+        }
+    }
 }
--- src/main/java/com/mycompany/healthcareapi/netbeans18/resources/PrescriptionResource.java
+++ src/main/java/com/mycompany/healthcareapi/netbeans18/resources/PrescriptionResource.java
@@ -4,10 +4,60 @@
  */
 package com.mycompany.healthcareapi.netbeans18.resources;
 
+import com.mycompany.healthcareapi.netbeans18.dao.PrescriptionDAO;
+import com.mycompany.healthcareapi.netbeans18.model.Prescription;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  *
  * @author THISAL
  */
+@Path("/prescriptions")
 public class PrescriptionResource {
-    
+    private PrescriptionDAO prescriptionDAO = new PrescriptionDAO();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Prescription> getAllPrescriptions() {
+        return prescriptionDAO.getAllPrescriptions();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Prescription getPrescription(@PathParam("id") int id) {
+        return prescriptionDAO.getPrescription(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Prescription createPrescription(Prescription prescription) {
+        prescriptionDAO.createPrescription(prescription);
+        return prescription;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Prescription updatePrescription(@PathParam("id") int id, Prescription prescription) {
+        prescriptionDAO.updatePrescription(prescription);
+        return prescription;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deletePrescription(@PathParam("id") int id) {
+        prescriptionDAO.deletePrescription(id);
+    }
 }
